2f750fa358196317ad1479d0a8368b886ff5a40f
Alex Coplan <lexy0202@gmail.com>
2020-03-30 22:42:03 +0100

    source tree reorg
     * For now, library stuff under base, and individual chapters under their own
       dirs.
     * TODO for tomorrow: fix the CI (no ml-lex by default on linux?)

--- artefacts.py
+++ artefacts.py
@@ -1,6 +1,6 @@
 from configure import BuildEnv
 
 def describe(env : BuildEnv) -> None:
-  env.Test('test_util', 'TestUtil.main', 'test_util.cm')
-  env.Test('test_chap1', 'TestChap1.main', 'test_chap1.cm')
+  env.Test('test_util', 'TestUtil.main', 'base/test_util.cm')
+  env.Test('test_chap1', 'TestChap1.main', 'chap1/test_chap1.cm')
   env.Test('test_lexer', 'TestLexer.main', 'chap2/test_lexer.cm')
